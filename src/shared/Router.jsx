b14c2ac3570d2682ea42c6f9f9d860a69a5bd075
ji min <142903244+gggimmmin@users.noreply.github.com>
2024-02-21 22:12:33 +0900

    [feat] Router 셋업 로그인 상태 수정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
 import Detail from "../pages/Detail";
+import Profile from "../pages/Profile";
 
 const Router = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="home" element={<Home />} />
-        <Route path="detail/:id" element={<Detail />} />
-        <Route path="*" element={<Navigate replace to="/" />} />
+        {isLogin ? (
+          <>
+            <Route path="/home" element={<Home />} />
+            <Route path="/detail/:id" element={<Detail />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Navigate replace to="/login" />} />
+          </>
+        )}
       </Routes>
     </BrowserRouter>
   );
